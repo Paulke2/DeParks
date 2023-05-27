d414b0f6e3a09a6972a452957ac0bc3814df916c
Paulke2 <paulke@udel.edu>
2023-05-27 15:40:58 -0400

    added schemas. server working

---
+++ server/routes/plants.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+//get all Plants
+router.get('/',(req,res)=>{
+
+});
+//getting one
+router.get('/:Plantid',(req,res)=>{
+
+});
+//creating one
+
+//deletingone
+
+//updating one
+
+module.exports=router;
